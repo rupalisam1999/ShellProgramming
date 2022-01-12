eedd0e1e6af6429d2a51c1e91335d3890afd2967
rupalisam1999 <rupalisangale1999@gmail.com>
2022-01-12 18:33:23 +0530

    Added: finding the greater number among two numbers

---
+++ selectionExample.sh
@@ -0,0 +1,12 @@
+#/bin/bash 
+
+var=10
+var=11
+if [ $var1 -ge $var2 ]
+then
+   echo "$var1 is greater than or equal to $var2"
+else
+   echo "$var2 is greater than $var1"
+fi
+
+#echo "last statement outside if condition"
