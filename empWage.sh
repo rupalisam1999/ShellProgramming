48796ef703c02e0a7e77642ad201dda0a14afbd3
rupalisam1999 <rupalisangale1999@gmail.com>
2022-01-15 12:40:46 +0530

    Added: calculated the salary for a month

--- empWage.sh
+++ empWage.sh
@@ -2,9 +2,13 @@
 
 isFullTime=1;
 isPartTime=2;
+totalsalary=0;
 empRatePerHrs=20;
-randomCheck=$((RANDOM%3));
-case $randomCheck in
+numWorkingDays=20;
+for (( day=1;day<=$numWorkingDays;day++ ))
+do
+   empCheck=$((RANDOM%3));
+   case $empCheck in
    $isFullTime)
       empHrs=8
       ;;
@@ -14,7 +18,9 @@ case $randomCheck in
    *)
       empHrs=0
       ;;
-esac
-
-salary=$(($empRatePerHrs*$empHrs));
-echo $salary;
+   esac
+   salary=$(($empRatePerHrs*$empHrs));
+   totalSalary=$(($totalSalary+$salary));
+done
+echo  $totalSalary
+echo  $empCheck
